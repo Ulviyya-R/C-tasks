cd28cd4cffc8c80d9dc572fef9a66cdce060d8d9
Ulviyya M. Rustamova <ulviyyamr@code.local>
2022-11-11 18:37:59 +0400

    This is C# task

---
+++ opentasks/opentasks/Program.cs
@@ -0,0 +1,26 @@
+﻿
+#region  2 nin quvveti 
+//System.Console.WriteLine("Please add number");
+
+//string NumSt = System.Console.ReadLine();
+
+//int num = System.Convert.ToInt32(NumSt);
+
+
+
+//while (num % 2 == 0)
+//{
+//    num /= 2;
+//}
+//if (num == 1)
+//{
+//    System.Console.WriteLine("This is power of 2");
+//}
+
+//else
+//{
+//    System.Console.WriteLine("This is not power of 2");
+//}
+
+#endregion
+
